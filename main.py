fac73ab183e4304845d9c1e21de7b2e284204407
EvickaStudio <kirenogfx@gmail.com>
2023-11-29 11:14:43 +0100

    Fixing text formatting/ filtering in the notification (might not be optimal for all applications)

--- main.py
+++ main.py
@@ -289,10 +289,16 @@ def parse_html_to_text(html):
     :return: Plain text after removing HTML tags.
     """
     try:
+        # TODO: FIlter mehr ausbauen, die letzten 4 Zeilen braucht man nicht
         soup = bs4.BeautifulSoup(html, "html.parser")
-        return "\n".join(
+        temp = "\n".join(
             [line.rstrip() for line in soup.get_text().splitlines() if line.strip()]
         )
+        # remove lines with more then 3 whitespaces in the beginning
+        temp = "\n".join(
+            [line for line in temp.splitlines() if not line.startswith("   ")]
+        )
+        return "\n".join(temp.splitlines()[:-1])
     except Exception as e:
         logging.exception("Failed to parse HTML")
         return None
