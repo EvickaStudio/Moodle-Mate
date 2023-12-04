b91f1db96607059bcf9f51fb0365db7738f3bae3
EvickaStudio <kirenogfx@gmail.com>
2023-12-04 14:14:38 +0100

    Add logging to chat_completion method and refactored the methode

--- gpt/openai_chat.py
+++ gpt/openai_chat.py
@@ -7,6 +7,7 @@ Github: @EvickaStudio
 """
 
 import openai  # version 1.3.6
+import logging
 
 
 class GPT:
@@ -19,20 +20,31 @@ class GPT:
 
     @apiKey.setter
     def apiKey(self, key: str) -> None:
+        """
+        Sets the API key and validates that it's not empty.
+
+        Args:
+            key (str): The API key for OpenAI.
+
+        Raises:
+            ValueError: If the API key is empty.
+        """
+        if not key:
+            raise ValueError("API key cannot be empty")
         self._apiKey = key
         openai.api_key = key
 
     def chat_completion(self, model: str, systemMessage: str, userMessage: str) -> str:
         """
-        Chat completion endpoint for OpenAI API
+        Chat completion endpoint for OpenAI API.
 
         Args:
-            model (str): The model to use for chat completion. Valid options are 'gpt-3.5-turbo' and 'gpt-4' for example.
+            model (str): The model to use for chat completion.
             systemMessage (str): The system message to provide context for the conversation.
             userMessage (str): The user message to generate a response.
 
         Returns:
-            str: The generated response from the chat completion.
+            str: The generated response from the chat completion, or None if an error occurs.
 
         Examples:
             ```python
@@ -47,18 +59,34 @@ class GPT:
             print(response)
             ```
         """
+        logging.info("Requesting chat completion from OpenAI")
         try:
             response = openai.chat.completions.create(
+
                 model=model,
                 messages=[
                     {
                         "role": "system",
-                        "content": systemMessage,  # System message set in config
-                    },
-                    {"role": "user", "content": userMessage},  # Moodle message input
+                        "content": systemMessage,
+                    },  # System message set in config
+                    {
+                        "role": "user",
+                        "content": userMessage,
+                    },  # User message set in config
                 ],
             )
             return response.choices[0].message.content
         except Exception as e:
-            print(f"An error occurred: {str(e)}")
+            logging.error(f"An unexpected error occurred: {str(e)}")
             return None
+
+
+# Example usage
+# gpt = GPT()
+# gpt.apiKey = "your_api_key"
+# response = gpt.chat_completion(
+#     model="gpt-3.5-turbo",
+#     systemMessage="You are a helpful assistant.",
+#     userMessage="How are you?"
+# )
+# print(response)
