6813472d7256fe904e98c7dacec1ec5d9d9b75c5
EvickaStudio <kirenogfx@gmail.com>
2023-12-07 16:33:40 +0100

    Refactored gpt/fakeopen_test.py

--- gpt/fakeopen_test.py
+++ gpt/fakeopen_test.py
@@ -5,7 +5,7 @@ gpt = FGPT()
 response = gpt.chat_completion(
     system_message="You are a helpful assistant.",
     user_message="How are you?",
-    model="gpt-4-1106-preview"
+    model="gpt-4-1106-preview",
 )
 print(response)
 # Thank you for asking! As a computer program, I don't have feelings, but I'm here and ready to assist you. How can I help you today?
