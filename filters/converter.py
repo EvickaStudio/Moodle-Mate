a7014ed4f29863c421ebf479431caaf54d029ee5
EvickaStudio <me@evicka.de>
2024-09-06 17:14:59 +0200

    Refactor convert.js: Update convert function to handle Node.js script path dynamically

--- filters/converter.py
+++ filters/converter.py
@@ -1,17 +1,29 @@
 import subprocess
 import os
 
+# Git Path
+SCRIPT_PATH = os.path.join("assets", "convert.js")
+
+
 def convert(html_content: str) -> str:
-    script_path = os.path.join("assets", "convert.js")
+    """
+    Converts HTML content to another format using a Node.js script (turndown).
+    Args:
+        html_content (str): The HTML content to be converted.
+    Returns:
+        str: The converted content.
+    Raises:
+        RuntimeError: If the Node.js script returns a non-zero exit code.
+    """
     result = subprocess.run(
-        ["node", script_path],
+        ["node", SCRIPT_PATH],
         input=html_content,
         text=True,
         capture_output=True,
-        encoding="utf-8"
+        encoding="utf-8",
     )
 
     if result.returncode != 0:
-        print(f"Error: {result.stderr}")
+        raise RuntimeError(result.stderr)
 
     return result.stdout
