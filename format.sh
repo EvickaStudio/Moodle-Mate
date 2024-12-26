95560b36d994f51bf8e91fdc78e947a43b778f10
evicka <me@evicka.de>
2024-12-26 12:21:32 +0100

    - enhance formatting script with flake8 checks and remove unnecessary sleep

--- format.sh
+++ format.sh
@@ -2,8 +2,8 @@
 
 isort .
 black .
-clear
-# wait for 1 second
-sleep 1
+black --check .
 flake8 .
+flake8 . --count --select=E9,F63,F7,F82 --show-source --statistics
+flake8 . --count --exit-zero --max-complexity=10 --max-line-length=127 --statistics
 mypy --ignore-missing-imports --exclude notification/discord.py --exclude utils/logo.py --exclude src/turndown/turndown.py --exclude example --exclude main.py --exclude moodle/moodle_notification_handler.py --follow-imports=skip .
