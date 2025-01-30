5ef9e07885695ac4984a8a1597b142ff5a5733cf
EvickaStudio <me@evicka.de>
2025-01-30 14:26:00 +0100

    - add some docs to converter.py

--- src/services/markdown/converter.py
+++ src/services/markdown/converter.py
@@ -2,7 +2,7 @@ import re
 
 from .turndown import MarkdownConverter
 
-td = MarkdownConverter({"headingStyle": "atx", "codeBlockStyle": "fenced"})
+TURNDOWN = MarkdownConverter({"headingStyle": "atx", "codeBlockStyle": "fenced"})
 
 
 def convert(html_content: str) -> str:
@@ -15,7 +15,7 @@ def convert(html_content: str) -> str:
     Returns:
         str: The converted and cleaned content.
     """
-    md_output = td.to_markdown(html_content)
+    md_output = TURNDOWN.to_markdown(html_content)
     return clean_converted_text(md_output)
 
 
@@ -29,7 +29,11 @@ def clean_converted_text(text: str) -> str:
     Returns:
         str: The cleaned text.
     """
+    # Regex to detect Markdown images
+    # as Discord does not support them, we remove them
     reg = r"\[!\[.*\]\(.*\)\]\(.*\)"
     text = re.sub(reg, "", text)
-    text = text.replace("* * *", "")
+    # Regex to detect horizontal rules
+    reg = r"* * *"
+    text = re.sub(reg, "", text)
     return text
